076bef264f344d05314c9d389ad8ed36a9fb2a02
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-05-13 01:58:13 +0700

    fix: formating files for PEP8

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -46,18 +46,18 @@ posts = [
 
 def index(request):
     template = 'blog/index.html'
-    context = { 'posts': reversed(posts) }
+    context = {'posts': reversed(posts)}
     return render(request, template, context)
 
 
 def post_detail(request, id):
     template = 'blog/detail.html'
     post = next((p for p in posts if p['id'] == id), None)
-    context = { 'post': post }
+    context = {'post': post}
     return render(request, template, context)
 
 
 def category_posts(request, category_slug):
     template = 'blog/category.html'
-    context = { 'category_slug': category_slug }
+    context = {'category_slug': category_slug}
     return render(request, template, context)
